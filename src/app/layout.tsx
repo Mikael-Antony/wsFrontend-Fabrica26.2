f50761456c8ed96d2f99f723064955f4f20a7592
MIKAEL ANTONY ALBUQUERQUE DA SILVA <mikael.silva006@cs.unipe.edu.br>
2026-08-31 15:55:11 -0300

    fix: correção de erro (## Error Type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+﻿import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
@@ -23,16 +23,16 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-      <Header />
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
+        <Header />
         <main className="min-h-screen flex flex-col items-center justify-center ">
           {children}
         </main>
         <Footer />
       </body>
-
     </html>
   );
 }
