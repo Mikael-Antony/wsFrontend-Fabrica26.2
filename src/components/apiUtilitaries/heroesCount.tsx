746a3073cb17073cd5ea759410c89497b27926ee
MIKAEL ANTONY ALBUQUERQUE DA SILVA <mikael.silva006@cs.unipe.edu.br>
2026-08-31 17:47:14 -0300

    feat: organizando a formatacao

--- src/components/apiUtilitaries/heroesCount.tsx
+++ src/components/apiUtilitaries/heroesCount.tsx
@@ -1,8 +1,6 @@
 import { overfastApi } from "@/services/overfastApi";
 
-/*
-  busca e conta quantos herois foram encontrados na requisicao api
-*/
+/* busca e conta quantos herois foram encontrados na requisicao api */
 export async function fetchHeroesCount() {
   const response = await overfastApi.get('/heroes');
   return response.data.length;
@@ -10,5 +8,9 @@ export async function fetchHeroesCount() {
 
 export default function HeroesCount() {
 
-  return <p className="text-sm text-gray-800 dark:text-gray-300">We have <span className="font-bold text-orange-500">{fetchHeroesCount()} heroes</span> available.</p>;
+  return (
+    <p className="text-sm text-gray-800 dark:text-gray-300">
+      We have <span className="font-bold text-orange-500">{fetchHeroesCount()} heroes</span> available.
+    </p>
+  )
 }
